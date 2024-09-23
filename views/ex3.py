c722dc48f43ad8a965615af94a874059f68cfeef
eunjian <oneandonlyjay9@gmail.com>
2024-09-23 10:53:14 +0900

    update files

--- views/ex3.py
+++ views/ex3.py
@@ -19,9 +19,20 @@ def createPage():
     
     container2 = st.container(border=True)
     container2.subheader('학습 모델 정보')
-    t1,t2, t3 = container2.tabs(['공통 파라미터', 'Neural Network', '학습상태'])
+    t1,t2, t3, t4 = container2.tabs(['공통 파라미터', '알고리즘 파라미터','Neural Network', '학습상태'])
+    with t4:
+        st.write('학습상태: 정상종료, 소요시간: 1분 [2019-05-13 16:54:03 ~ 2019-05-13 16:55:03]')
+        i1, i2, i3, i4 = st.columns(4)
+        with i1:
+            st.image('./accuracy.png')
+        with i2:
+            st.image('./loss.png')
+        with i3:
+            st.image('./confusion_matrix.png')
+        with i4:
+            st.image('./precision_recall_f1.png')
     
-    _,col1,col2,col3,col4,col5,col6 = st.columns([5,1,1,1,1,1,1])
+    _,col1,col2,col3,col4,col5,col6 = st.columns([5,1,1,0.7,0.7,0.7,0.7])
     with col1:
         st.button('목록으로',type='primary')
     with col2:
